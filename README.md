026b4648f1ee3d28c3e2391d56e3e41f6564c997
Rena Shah <rena.shah@microsoft.com>
2020-03-20 10:30:20 -0700

    Initial push

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# AzureFilesBackupToAnotherFileShare
- 
+# AzCopy based Azure Files Backup To Another FileShare
+
+Many Azure Files customers who service, choose to implement a backup solution to another file share to be able to recover from unintended changes or deletions to their file systems. Azure Bakup solution today, is snapshot based. Which means that it will store data in same file share. This solution will enable you to create your own easy backup solution that automatically creates incremental backups of an Azure Files system on a customer-defined schedule. This webpage provides an overview of the Azure Files AzCopy based backup solution's design and functionality.
+
+
+## Solution overview
+This solution utilizes AzCopy - an purpose-built tool optimized for Azure Storage data movement needs. AzCopy is a command-line utility that you can use to copy blobs or files to or from a storage account. This solution copies snapshots from one file share to the other to ensure fast backups with minimal space overhead. Only changes will be at copied every backup. The copy happens on server side ensuring that it is fast and has minimal egress. This solution utilizes familiar technologies like Windows task Scheduler and Powershell making it easy to maintain without spending time on ramp-up.
+
+## Advantages
+* Space efficiency
+* Cost Efficiency
+* Speed
+* Familiar tooling
+
+## Limitations
+* There is no exhausive perf testing done and this solution only works on low churn datasets.
+* There is no CSS support on this solution.
+
+## Contributions
+* This is an open-source community maintained project and we welcome direct contributions to this project.
---
+++ backup.ps1
@@ -0,0 +1,71 @@
+﻿Write-Host "Starting ...."
+Install-Module -Name Az -AllowClobber -Scope AllUsers
+# Connect to Azure
+Connect-AzAccount
+# List Azure Subscriptions
+Get-AzSubscription
+
+# Define Variables
+$targetsubscriptionId = "dd80b94e-0463-4a65-8d04-c94f403879dc"
+$sourcesubscriptionId = "dd80b94e-0463-4a65-8d04-c94f403879dc"
+$targetstorageAccountRG = "renashazcopyrg"
+$sourcestorageAccountRG = "renashazcopyrg"
+$targetstorageAccountName = "renashtargetazcopy"
+$sourcestorageAccountName = "renashtargetazcopy"
+$targetstoragefileshareName = "faketarget"
+$sourcestoragefileshareName = "sourcefileshare"
+$azcopypath = ".\azcopy"
+
+
+
+# SOURCE CONTEXT
+
+
+# Select right Azure Subscription
+Select-AzSubscription -subscriptionId $sourcesubscriptionId
+
+# Get Storage Account Key
+$sourcestorageAccountKey = (Get-AzStorageAccountKey -ResourceGroupName $sourcestorageAccountRG -Name $sourcestorageAccountName).Value[0]
+
+# Set AzStorageContext
+$sourceContext = New-AzStorageContext -StorageAccountKey $sourcestorageAccountKey -StorageAccountName $sourcestorageAccountName
+
+# Snapshot source share
+$sourceshare = Get-AzStorageShare -Context $sourceContext.Context -Name $sourcestoragefileshareName
+$sourcesnapshot = $sourceshare.Snapshot()
+
+# Generate source Snapshot SAS URI
+
+# $sourceSASURI = New-AzStorageShareSASToken -Context $destinationContext -ExpiryTime(get-date).AddMonths(60) -FullUri -Name $sourcesnapshot.SnapshotQualifiedUri.PathAndQuery.Substring(1) -Permission rwdl -StartTime (get-date).AddSeconds(-100)
+# $sourceSASURI = New-AzStorageShareSASToken -Context $destinationContext -ExpiryTime(get-date).AddMonths(60) -FullUri -Name $sourcestoragefileshareName -Permission rcwdl -StartTime (get-date).AddSeconds(-100)
+$sourceSASURIBasePermission = New-AzStorageAccountSASToken -Context $destinationContext -Service File -ResourceType Service,Container,Object -Permission "racwdlup"  -ExpiryTime (get-date).AddMonths(60) -StartTime (get-date).AddSeconds(-100)
+$sourceSASURI = $sourceContext.FileEndPoint.ToString() + $sourceSASURIBasePermission.Replace('?',($sourcesnapshot.SnapshotQualifiedUri.PathAndQuery.Substring(1) + "&"))
+
+# TARGET
+
+# Select right Azure Subscription
+Select-AzSubscription -subscriptionId $targetsubscriptionId
+
+# Get Storage Account Key
+$targetstorageAccountKey = (Get-AzStorageAccountKey -ResourceGroupName $targetstorageAccountRG -Name $targetstorageAccountName).Value[0]
+
+# Set AzStorageContext
+$destinationContext = New-AzStorageContext -StorageAccountKey $targetstorageAccountKey -StorageAccountName $targetstorageAccountName
+
+
+# Generate target SAS URI
+$targetSASURI = New-AzStorageShareSASToken -Context $destinationContext -ExpiryTime(get-date).AddMonths(60) -FullUri -Name $targetstoragefileshareName -Permission rcwdl
+
+
+# TODO - logic to download newer copy of azcopy when available
+
+# Upload File using AzCopy
+# F:\azcopy_windows_amd64_10.3.3\azcopy_windows_amd64_10.3.3\azcopy sync $sourceSASURI $targetSASURI
+
+$AzCopyCmd= $azcopypath+"\azcopy.exe sync " + $sourceSASURI + "  " +$targetSASURI
+
+Invoke-Expression -command $AzCopyCmd
+
+# Snapshot target share
+$targetshare = Get-AzStorageShare -Context $targetContext.Context -Name $targetstoragefileshareName
+$targetshare.Snapshot()
