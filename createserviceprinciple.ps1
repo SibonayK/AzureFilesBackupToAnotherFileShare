5d14a3f4be5ed9589ceda45dbee9e53797eb7e21
Rena Shah <rena.shah@microsoft.com>
2020-03-19 20:32:42 -0700

    Add CreateServicePrinciple powershell script

---
+++ createserviceprinciple.ps1
@@ -0,0 +1,15 @@
+$sourcesubscriptionId = "dd80b94e-0463-4a65-8d04-c94f403879dc"
+$appidname = "renashazcopyapp"
+$apppassword = "MyVerySecurePassword1234!"
+
+
+
+
+Select-AzSubscription -subscriptionId $sourcesubscriptionId
+Import-Module Az.Resources # Imports the PSADPasswordCredential object
+$credentials = New-Object Microsoft.Azure.Commands.ActiveDirectory.PSADPasswordCredential -Property @{ StartDate=Get-Date; EndDate=Get-Date -Year 2024; Password=$apppassword}
+$sp = New-AzAdServicePrincipal -DisplayName $appidname -PasswordCredential $credentials
+Write-Output $sp.DisplayName
+Write-Output $sp.Id
+$tenantId = (Get-AzContext).Tenant.Id
+Write-Output $tenantId
