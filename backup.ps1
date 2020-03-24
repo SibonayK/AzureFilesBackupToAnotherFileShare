68db9b278e5fe837314113b1e080eed894c2bf6f
Rena Shah <rena.shah@microsoft.com>
2020-03-23 23:25:09 -0700

    FitAndFinishBackupAndSPCreationScript
    Works 100%

--- backup.ps1
+++ backup.ps1
@@ -1,27 +1,28 @@
-﻿Write-Host "Starting ...."
-Install-Module -Name Az -AllowClobber -Scope AllUsers
+﻿
+
+Write-Host "Starting ...."
+#Install-Module -Name Az -AllowClobber -Scope AllUsers
 
 # Define Variables
-$targetsubscriptionId = "dd80b94e-0463-4a65-8d04-c94f403879dc"
-$sourcesubscriptionId = "dd80b94e-0463-4a65-8d04-c94f403879dc"
-$targetstorageAccountRG = "renashazcopyrg"
-$sourcestorageAccountRG = "renashazcopyrg"
-$targetstorageAccountName = "renashtargetazcopy"
-$sourcestorageAccountName = "renashtargetazcopy"
-$targetstoragefileshareName = "faketarget"
-$sourcestoragefileshareName = "sourcefileshare"
+$targetsubscriptionId = "7823e5c9-9c8d-4214-b814-e18363bfb850"
+$sourcesubscriptionId = "7823e5c9-9c8d-4214-b814-e18363bfb850"
+$targetstorageAccountRG = "renashdiybackuprg"
+$sourcestorageAccountRG = "renashdiybackuprg"
+$targetstorageAccountName = "renashdiybackupsa"
+$sourcestorageAccountName = "renashdiybackupsa"
+$targetstoragefileshareName = "target"
+$sourcestoragefileshareName = "source"
 $azcopypath = ".\azcopy"
+# TODO - Change to cert based auth - https://docs.microsoft.com/en-us/azure/active-directory/develop/howto-authenticate-service-principal-powershell#provide-certificate-through-automated-powershell-script-
 $spAppPassword = "myVerySecurePassword1234!"
-$spTenatId = "72f988bf-86f1-41af-91ab-2d7cd011db47"
-$spAppId = "9c792091-549a-4463-a00c-3c4badb67f19"
+$spTenatId = "475ce392-90cc-4e97-94b5-028213916c6f"
+$spAppId = "4bfba36a-e902-4ede-8f7a-22968dc8bf63"
 
+# SOURCE
 # Connect to Azure
 $passwd = ConvertTo-SecureString $spAppPassword -AsPlainText -Force
 $pscredential = New-Object System.Management.Automation.PSCredential($spAppId, $passwd)
-Connect-AzAccount -ServicePrincipal -Credential $pscredential -Tenant $spTenatId
-
-# Select right Azure Subscription
-Select-AzSubscription -subscriptionId $sourcesubscriptionId
+Connect-AzAccount -ServicePrincipal -Credential $pscredential -Tenant $spTenatId -Subscription $sourcesubscriptionId
 
 # Get Storage Account Key
 $sourcestorageAccountKey = (Get-AzStorageAccountKey -ResourceGroupName $sourcestorageAccountRG -Name $sourcestorageAccountName).Value[0]
@@ -34,14 +35,10 @@ $sourceshare = Get-AzStorageShare -Context $sourceContext.Context -Name $sources
 $sourcesnapshot = $sourceshare.Snapshot()
 
 # Generate source Snapshot SAS URI
-
-# $sourceSASURI = New-AzStorageShareSASToken -Context $destinationContext -ExpiryTime(get-date).AddMonths(60) -FullUri -Name $sourcesnapshot.SnapshotQualifiedUri.PathAndQuery.Substring(1) -Permission rwdl -StartTime (get-date).AddSeconds(-100)
-# $sourceSASURI = New-AzStorageShareSASToken -Context $destinationContext -ExpiryTime(get-date).AddMonths(60) -FullUri -Name $sourcestoragefileshareName -Permission rcwdl -StartTime (get-date).AddSeconds(-100)
 $sourceSASURIBasePermission = New-AzStorageAccountSASToken -Context $destinationContext -Service File -ResourceType Service,Container,Object -Permission "racwdlup"  -ExpiryTime (get-date).AddMonths(60) -StartTime (get-date).AddSeconds(-100)
 $sourceSASURI = $sourceContext.FileEndPoint.ToString() + $sourceSASURIBasePermission.Replace('?',($sourcesnapshot.SnapshotQualifiedUri.PathAndQuery.Substring(1) + "&"))
 
 # TARGET
-
 # Select right Azure Subscription
 Select-AzSubscription -subscriptionId $targetsubscriptionId
 
@@ -56,15 +53,16 @@ $destinationContext = New-AzStorageContext -StorageAccountKey $targetstorageAcco
 $targetSASURI = New-AzStorageShareSASToken -Context $destinationContext -ExpiryTime(get-date).AddMonths(60) -FullUri -Name $targetstoragefileshareName -Permission rcwdl
 
 
-# TODO - logic to download newer copy of azcopy when available
+# logic to download newer copy of azcopy when available
+# Invoke-WebRequest https://azcopyvnext.azureedge.net/release20190517/azcopy_windows_amd64_10.1.2.zip -OutFile azcopyv10.zip $azcopypath
 
 # Upload File using AzCopy
-# F:\azcopy_windows_amd64_10.3.3\azcopy_windows_amd64_10.3.3\azcopy sync $sourceSASURI $targetSASURI
-
-$AzCopyCmd= $azcopypath+"\azcopy.exe sync " + $sourceSASURI + "  " +$targetSASURI
-
-Invoke-Expression -command $AzCopyCmd
+Set-Location -Path $azcopypath
+$azcopyout = .\azcopy.exe sync $sourceSASURI $targetSASURI
+Write-Output $azcopyout
 
 # Snapshot target share
 $targetshare = Get-AzStorageShare -Context $targetContext.Context -Name $targetstoragefileshareName
 $targetshare.Snapshot()
+
+# TODO - For standardization, make error handling, logging, parameterization and retries like in https://github.com/Azure/azure-docs-powershell-samples/blob/master/storage/migrate-blobs-between-accounts/migrate-blobs-between-accounts.ps1
--- createserviceprinciple.ps1
+++ createserviceprinciple.ps1
@@ -1,12 +1,14 @@
-$sourcesubscriptionId = "dd80b94e-0463-4a65-8d04-c94f403879dc"
+# TODO - Change to cert based auth instead of password based auth  - https://docs.microsoft.com/en-us/azure/active-directory/develop/howto-authenticate-service-principal-powershell#next-steps
+$sourcesubscriptionId = "7823e5c9-9c8d-4214-b814-e18363bfb850"
 $appidname = "renashdiyfilesbkapp"
 $apppassword = "myVerySecurePassword1234!"
-
+# Install-Module -Name Az -AllowClobber -Scope AllUsers
+Connect-AzAccount
 Select-AzSubscription -subscriptionId $sourcesubscriptionId
 Import-Module Az.Resources # Imports the PSADPasswordCredential object
 $credentials = New-Object Microsoft.Azure.Commands.ActiveDirectory.PSADPasswordCredential -Property @{ StartDate=Get-Date; EndDate=Get-Date -Year 2024; Password=$apppassword}
 $sp = New-AzAdServicePrincipal -DisplayName $appidname -PasswordCredential $credentials
 Write-Output $sp.DisplayName
-Write-Output $sp.Id
+Write-Output $sp.ApplicationId
 $tenantId = (Get-AzContext).Tenant.Id
 Write-Output $tenantId
