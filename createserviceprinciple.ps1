881f26516239632a1fe1e7a03403fe1ea3115353
SibonayK <41348468+SibonayK@users.noreply.github.com>
2020-10-08 23:26:21 -0700

    update SP script to use cert-based auth

--- createserviceprinciple.ps1
+++ createserviceprinciple.ps1
@@ -1,13 +1,24 @@
-# TODO - Change to cert based auth instead of password based auth  - https://docs.microsoft.com/en-us/azure/active-directory/develop/howto-authenticate-service-principal-powershell#next-steps
-$sourcesubscriptionId = "7823e5c9-9c8d-4214-b814-e18363bfb850"
-$appidname = "renashdiyfilesbkapp"
-$apppassword = "myVerySecurePassword1234!"
-# Install-Module -Name Az -AllowClobber -Scope AllUsers
+$sourcesubscriptionId = <sourceSubscriptionID>
+$appidname = <AppID>
+
+# Uncomment the line below if Az is not installed yet
+#Install-Module -Name Az -AllowClobber -Scope AllUsers
 Connect-AzAccount
 Select-AzSubscription -subscriptionId $sourcesubscriptionId
-Import-Module Az.Resources # Imports the PSADPasswordCredential object
-$credentials = New-Object Microsoft.Azure.Commands.ActiveDirectory.PSADPasswordCredential -Property @{ StartDate=Get-Date; EndDate=Get-Date -Year 2024; Password=$apppassword}
-$sp = New-AzAdServicePrincipal -DisplayName $appidname -PasswordCredential $credentials
+
+# Create service principal using certificate-based authentication
+$cert = New-SelfSignedCertificate -CertStoreLocation "cert:\CurrentUser\My"
+  -Subject "CN=exampleappScriptCert" `
+  -KeySpec KeyExchange
+$keyValue = [System.Convert]::ToBase64String($cert.GetRawCertData())
+
+$sp = New-AzADServicePrincipal -DisplayName $appidname 
+-CertValue $keyValue 
+Sleep 20
+New-AzRoleAssignment -RoleDefinitionName Contributor -ServicePrincipalName $sp.ApplicationId
+
+# These values will be used to authenticate in the Backup script
+Write-Output $cert.Thumbprint 
 Write-Output $sp.DisplayName
 Write-Output $sp.ApplicationId
 $tenantId = (Get-AzContext).Tenant.Id
